32b772d733e374574c99894d0acf3f9adb78b025
cenicerolleno <mrnolan88@gmail.com>
2026-07-10 21:06:17 +0200

    Add auth blueprint, signup endpoint and bcrypt

--- backend/api/auth/routes.py
+++ backend/api/auth/routes.py
@@ -1,11 +1,39 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from extensions import bcrypt, jwt, db
 from models import User
+from sqlalchemy.exc import IntegrityError
 
 
 auth_bp = Blueprint('auth', __name__)
 
-@auth_bp.route('/register', methods=['POST'])
-def register():
-    # Implement registration logic here
-    return {"message": "register endpoint working"}, 200
+@auth_bp.route('/signup', methods=['POST'])
+def signup():
+
+    body = request.get_json(silent=True )
+    
+    if body is None:
+        return jsonify({'msg': 'Debes enviar informacion en el body'}), 400
+    
+    if 'username' not in body:
+        return jsonify({'msg': 'Debes proporcionar un nombre de usuario'}), 400
+    
+    if 'email' not in body:
+        return jsonify({'msg': 'Debes proporcionar un correo electrónico'}), 400
+    if 'password' not in body:
+        return jsonify({'msg': 'Debes proporcionar una contraseña'}), 400
+    
+    new_register = User()
+    new_register.username = body['username']
+    new_register.email = body['email']
+    hash_password = bcrypt.generate_password_hash(body['password']).decode('utf-8')
+    new_register.password_hash = hash_password
+    new_register.is_active = True
+    
+    try:
+        db.session.add(new_register)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({'msg': 'Ya existe un usuario con ese email'}), 400
+   
+    return jsonify({'msg': 'Usuario registrado!', 'register': new_register.serialize()}), 200
